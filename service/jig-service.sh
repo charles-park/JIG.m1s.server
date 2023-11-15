364b4aaf821d926a44e76f217310627c2acc943d
charles.park <charles.park@hardkernel.com>
2023-11-15 08:21:30 +0000

    remove iperf3_odroid

--- service/jig-service.sh
+++ service/jig-service.sh
@@ -6,11 +6,6 @@
 sleep 10 && sync
 
 #--------------------------
-# ODROID-M1S Server enable (iperf3 odroid iperf3 demon mode)
-#--------------------------
-/usr/bin/iperf3_odroid -s -p 8000 &
-
-#--------------------------
 # ODROID-M1S Server app
 #--------------------------
 /root/JIG.m1s.server/JIG.m1s.server > /dev/null 2>&1
